aa4cd103833cd7ee5c117db02853982b22e1def7
Jeremy Elmani <jeremyelmani@gmail.com>
2025-11-14 17:37:20 -0500

    finish up back prop

--- layer.py
+++ layer.py
@@ -6,21 +6,43 @@ class Layer:
     def __init__(self, activation: str, units: int):
         self.activation = activation
         self.units = units
+        self.alpha = 0.001
         return self
     
     def set_input(self, input: int):
         self._w = np.zeros(shape=(input, self.units))
         self._b = np.zeros(shape=self.units)
     
-    def output(self, input):
+    def forward(self, input):
+        self.input = input
         z = (input @ self._w) + self._b
         match self.activation:
             case "linear":
                 return z
             case "sigmoid":
-                return 1 / (1 + np.exp(-z))
+                self.output = 1 / (1 + np.exp(-z))
+                return self.output
             case "relu":
-                return np.maximum(0, z)
+                self.output = np.maximum(0, z)
+                return self.output
             case "softmax":
                 e_z = np.exp(z)
-                return e_z / np.sum(e_z, axis=-1) 
+                self.output = e_z / np.sum(e_z, axis=-1)
+                return self.output
+            
+    def backward(self, dj_da):
+        match self.activation:
+            case "linear":
+                dj_dz = dj_da
+            case "sigmoid":
+                dj_dz = dj_da * (self.output * (1 - self.output))
+            case "relu":
+                dj_dz = dj_da * (self.output > 0)
+            case "softmax":
+                dj_dz = dj_da
+        dj_dw = self.input.T @ dj_dz
+        dj_db = np.sum(dj_dz, axis=0, keepdims=True)
+        dj_da_prev = dj_dz @ self._w.T
+        self._w -= self.alpha * dj_dw
+        self._b -= self.alpha * dj_db
+        return dj_da_prev
--- sequential.py
+++ sequential.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 class Sequential:
     def __init__(self, layers: list):
         self.layers = layers
@@ -10,21 +9,46 @@ class Sequential:
     def compile(self, loss: str):
         match loss:
             case "MSE":
-                pass
+                self.loss = self.compute_mse
             case "BinaryCrossEntropy":
-                pass
+                self.loss = self.compute_binary_ce
+            case "SparseCrossEntropy":
+                self.loss = self.compute_sparse_se
     
-    def compute_mse(self, X, y_target):
-        return np.square(self.predict(X) - y_target) / (2 * X.shape[0]) 
+    def compute_mse(self, X, y_target, derivative: bool = False):
+        resid = self.predict(X) - y_target
+        if derivative:
+            return np.mean(resid, axis=0)
+        return 0.5 * np.mean(resid**2)
     
-    def compute_binary_ce(self, X, y_target):
-        pass
+    
+    def compute_binary_ce(self, X, y_target, derivative: bool = False):
+        y_pred = self.predict(X)
+        resid = y_pred - y_target
+        if derivative:
+            return np.mean(resid, axis=0)
+        return np.mean(-y_target * np.log(y_pred) - (1 - y_target) * np.log(1 - y_pred))
 
-    def compute_sparse_se(self, X, y_target):
-        pass
+    def compute_sparse_se(self, X, y_target, derivative: bool = False):
+        y_pred = self.predict(X)
+        if derivative:
+            m = y_target.shape[0]
+            num_classes = y_pred.shape[1]
+            Y = np.zeros((m, num_classes))
+            Y[np.arange(m), y_target] = 1
+            return (y_pred - Y) / m
+        N = y_target.astype(int).ravel()
+        return np.mean(-np.log(y_pred[:, N]))
     
     def predict(self, X):
-        a = self.layers[0].output(X)
+        a = self.layers[0].forward(X)
         for i in range(1, len(self.layers)):
-            a = self.layers[i].output(a)
+            a = self.layers[i].forward(a)
         return a
+    
+    def fit(self, X, y_target):
+        self.layers[0].set_input(X.shape[1])
+        # Backpropagation
+        dj_da = self.loss(X, y_target, derivative=True)
+        for i in range(len(self.layers) - 1, -1, -1):
+            dj_da = self.layers[i].backward(dj_da)
